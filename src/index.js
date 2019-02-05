fefe9773d92997d9bb26c21c070be4cda2d3659a
pdronenko <pdronenko@ya.ru>
2019-02-05 11:16:20 +0300

    delete return

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import readlineSync from 'readline-sync';
 export default () => {
   console.log('Welcome to the Brain Games!\n');
   const name = readlineSync.question('May I have your name? ');
-  return console.log(`Hello, ${name}!`);
+  console.log(`Hello, ${name}!`);
 };
