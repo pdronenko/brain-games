793f5db63246dd25d8814c7289b0adf8969f1b58
pdronenko <pdronenko@ya.ru>
2019-02-06 14:06:53 +0300

    add working brain-calc function

--- src/games/brain-calc-game.js
+++ src/games/brain-calc-game.js
@@ -1,4 +1,33 @@
 import { generateRandomNum, brainGameEngine } from '..';
 import { cons } from 'hexlet-pairs';
 
+const description = 'What is the result of the expression?';
+const operation = (num1, num2, operator) => {
+  switch (operator) {
+    case '+':
+      return num1 + num2;
+    case '-':
+      return num1 - num2;
+    case '*':
+      return num1 * num2;
+    default:
+      return 'it is not operator!';
+  }
+};
+
+const brainCalcGame = () => {
+  const minNum = 1;
+  const maxNum = 100;
+  const maxNumOfOperator = 4;
+  const randomNum1 = generateRandomNum(minNum, maxNum);
+  const randomNum2 = generateRandomNum(minNum, maxNum);
+
+  const operatorNum = generateRandomNum(minNum, maxNumOfOperator);
+  const operator = operatorNum === 1 ? '+' : operatorNum === 2 ? '-' : '*';
+
+  const question = `${randomNum1} ${operator} ${randomNum2}`;
+  const correctAnswer = `${operation(randomNum1, randomNum2, operator)}`;
+  return cons(description, cons(question, correctAnswer));
+};
+
 export default () => brainGameEngine(brainCalcGame);
