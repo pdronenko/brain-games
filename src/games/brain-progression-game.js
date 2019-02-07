2cf280c07f9c00ba004985baa54175ff1a5ebfd5
pdronenko <pdronenko@ya.ru>
2019-02-07 15:47:53 +0300

    add progression logic

--- src/games/brain-progression-game.js
+++ src/games/brain-progression-game.js
@@ -2,12 +2,34 @@ import { brainGameEngine } from '..';
 import generateRandomNum from '../utils';
 import { cons } from 'hexlet-pairs';
 
-const description = '';
+const description = 'What number is missing in the progression?';
+const minNum = 1;
+const maxNum = 11;
+let answerNum = 0;
 
-const generateProgressionGameInfo = () => {
-  const question = '';
-  const correctAnswer = '';
+const generateProg = (progStartNum, constant, questionNum) => {
+  const iter = (num, str, counter) => {
+    if (counter === 0) {
+      return str;
+    }
+    let string = `${str} ${num}`;
+    if (counter === questionNum) {
+      answerNum = num;
+      string = `${str} ..`;
+    }
+    return iter(num + constant, string, counter - 1);
+  };
+  return iter(progStartNum, '', maxNum - 1);
+};
+
+const generateProgGameInfo = () => {
+  const constant = generateRandomNum(minNum, maxNum);
+  const progStartNum = generateRandomNum(minNum, maxNum);
+  const questionNum = generateRandomNum(minNum, maxNum);
+
+  const question = generateProg(progStartNum, constant, questionNum);
+  const correctAnswer = `${answerNum}`;
   return cons(question, correctAnswer);
 };
 
-export default () => brainGameEngine(description, generateProgressionGameInfo);
+export default () => brainGameEngine(description, generateProgGameInfo);
