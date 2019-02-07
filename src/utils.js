8b1be8dc4059a95c696eb807b68970d2920ae65c
pdronenko <pdronenko@ya.ru>
2019-02-07 15:48:54 +0300

    random now include min and max numbers

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,3 @@
 // Random number generator
 // minNumber used for avoid zero in randomFunction result
-export default (min, max) => Math.floor(Math.random() * (max - min) + min);
+export default (min, max) => Math.floor(min + Math.random() * (max + 1 - min));
