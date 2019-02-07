cf27b22ac30c4a4bc9c424da294dc37e308e99f8
pdronenko <pdronenko@ya.ru>
2019-02-07 13:04:52 +0300

    add gcd brain game logic

--- src/games/brain-gcd-game.js
+++ src/games/brain-gcd-game.js
@@ -1,11 +1,28 @@
 import { brainGameEngine } from '..';
+import generateRandomNum from '../utils';
 import { cons } from 'hexlet-pairs';
 
-const description = '';
+const description = 'Find the greatest common divisor of given numbers.';
+const minNum = 20;
+const maxNum = 100;
+
+const getGcd = (smallerNum, biggestNum) => {
+  if (biggestNum % smallerNum === 0) {
+    return smallerNum;
+  }
+
+  return getGcd(smallerNum - 1, biggestNum);
+};
 
 const generateGcdGameInfo = () => {
-  const question = '';
-  const correctAnswer = '';
+  const randomNum1 = generateRandomNum(minNum, maxNum);
+  const randomNum2 = generateRandomNum(minNum, maxNum);
+
+  const smallerNum = Math.min(randomNum1, randomNum2);
+  const biggestNum = Math.max(randomNum1, randomNum2);
+
+  const question = `${randomNum1} ${randomNum2}`;
+  const correctAnswer = `${getGcd(smallerNum, biggestNum)}`;
   return cons(question, correctAnswer);
 };
 
