47600f0b94d4d95217891d112129ab5844d6e6e8
pdronenko <pdronenko@ya.ru>
2019-02-07 12:26:34 +0300

    gcd brain game init

---
+++ src/games/brain-gcd-game.js
@@ -0,0 +1,12 @@
+import { brainGameEngine } from '..';
+import { cons } from 'hexlet-pairs';
+
+const description = '';
+
+const generateGcdGameInfo = () => {
+  const question = '';
+  const correctAnswer = '';
+  return cons(question, correctAnswer);
+};
+
+export default () => brainGameEngine(description, generateGcdGameInfo);
