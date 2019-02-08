8e0d5e258a3477fba18d002df182e13744905c29
pdronenko <pdronenko@ya.ru>
2019-02-08 13:35:44 +0300

    refactor code

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,31 +3,31 @@ import generateRandomNum from '../utils';
 import { cons, car, cdr } from 'hexlet-pairs';
 
 const description = 'What number is missing in the progression?';
-const minNum = 1;
-const maxNum = 10;
-const startPosition = 1;
+const minNum = 0;
+const minStepNum = 1;
+const maxNum = 9;
 const finishPosition = 10;
 const getAnswer = info => car(info);
 const getProgString = info => cdr(info);
 
 const generateProgAndAnswer = (progStartNum, progStep, hiddenNumPosition) => {
-  let currentNum = progStartNum;
   let answerNum = 0;
   let progString = '';
 
-  for (let i = startPosition; i <= finishPosition; i += 1, currentNum += progStep) {
-    if (i !== hiddenNumPosition) progString = `${progString} ${currentNum}`;
-    else {
-      answerNum = currentNum;
-      progString = `${progString} ..`;
+  for (let i = 0; i < finishPosition; i += 1) {
+    let nextStepNum = progStartNum + (progStep * i);
+    if (i === hiddenNumPosition) {
+      answerNum = nextStepNum;
+      nextStepNum = '..';
     }
+    progString = `${progString} ${nextStepNum}`;
   }
   return cons(answerNum, progString);
 };
 
 const generateProgGameInfo = () => {
   const progStartNum = generateRandomNum(minNum, maxNum);
-  const progStep = generateRandomNum(minNum, maxNum);
+  const progStep = generateRandomNum(minStepNum, maxNum);
   const hiddenNumPosition = generateRandomNum(minNum, maxNum);
 
   const progressionInfo = generateProgAndAnswer(progStartNum, progStep, hiddenNumPosition);
