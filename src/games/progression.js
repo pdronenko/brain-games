7ad750a76425b03a4884ddf8d0c1588ecdcb76f1
pdronenko <pdronenko@ya.ru>
2019-02-08 19:20:17 +0300

    change const name

--- src/games/progression.js
+++ src/games/progression.js
@@ -6,16 +6,16 @@ const description = 'What number is missing in the progression?';
 const minNum = 0;
 const minStepNum = 1;
 const maxNum = 9;
-const finishPosition = 10;
+const lastNumInProg = 10;
 
 const generateProgString = (progStartNum, progStep, hiddenNumPosition) => {
   let progString = '';
 
-  for (let i = 0; i < finishPosition; i += 1) {
+  for (let i = 0; i < lastNumInProg; i += 1) {
     const nextStepNum = i !== hiddenNumPosition ? progStartNum + (progStep * i) : '..';
     progString = `${progString} ${nextStepNum}`;
   }
-  
+
   return progString;
 };
 
